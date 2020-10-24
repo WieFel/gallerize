b2da027fcb5eb471f49159660aaeb1d88c9ca3f1
Felix Wielander <felix.wielander@gmail.com>
2020-10-24 09:46:27 +0200

    Example project basically works

---
+++ example/lib/my_fancy_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MyFancyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("My Fancy Widget!!!"),
+    );
+  }
+}
