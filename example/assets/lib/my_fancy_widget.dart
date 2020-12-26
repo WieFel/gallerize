eb01c7c94a4bc1b212a3df7c6133a1e1d87116b7
WieFel <felix.wielander@gmail.com>
2020-12-26 15:26:31 +0100

    Added example web project build

---
+++ example/assets/lib/my_fancy_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyFancyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: Text("My Fancy Widget!"),
+      ),
+    );
+  }
+}
